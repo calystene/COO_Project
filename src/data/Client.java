cc5da23cfbe5c3a6a172831653a6375a18f1e6e8
Calystene <pierard.t@gmail.com>
2014-11-25 19:10:31 +0000

    Ajout des forfait la classe Client

--- src/data/Client.java
+++ src/data/Client.java
@@ -1,102 +1,136 @@
 package data;
 
+import java.util.ArrayList;
+
+import data.forfait.Forfait;
+
 /**
  * Class Client
  */
 public class Client {
 
-  //
-  // Fields
-  //
+	//
+	// Fields
+	//
 
-  private String prenom;
-  private String nom;
-  private int numero;
-  private CarteFidelite carte;
-  
-  //
-  // Constructors
-  //
-  public Client (String p, String no, int nu) { 
-	  prenom = p;
-	  nom = no;
-	  numero = nu;
-  };
-  
-  //
-  // Methods
-  //
+	private String prenom;
+	private String nom;
+	private int numero;
+	private CarteFidelite carte;
+	private ArrayList<Forfait> lesForfaits;
 
-  //
-  // Accessor methods
-  //
+	//
+	// Constructors
+	//
+	public Client(String p, String no, int nu) {
+		prenom = p;
+		nom = no;
+		numero = nu;
+		lesForfaits = new ArrayList<Forfait>();
+	};
 
-  /**
-   * Set the value of prenom
-   * @param newVar the new value of prenom
-   */
-  public void setPrenom (String newVar) {
-    prenom = newVar;
-  }
+	//
+	// Methods
+	//
 
-  /**
-   * Get the value of prenom
-   * @return the value of prenom
-   */
-  public String getPrenom () {
-    return prenom;
-  }
+	//
+	// Accessor methods
+	//
 
-  /**
-   * Set the value of nom
-   * @param newVar the new value of nom
-   */
-  public void setNom (String newVar) {
-    nom = newVar;
-  }
+	/**
+	 * Set the value of prenom
+	 * 
+	 * @param newVar
+	 *            the new value of prenom
+	 */
+	public void setPrenom(String newVar) {
+		prenom = newVar;
+	}
 
-  /**
-   * Get the value of nom
-   * @return the value of nom
-   */
-  public String getNom () {
-    return nom;
-  }
+	/**
+	 * Get the value of prenom
+	 * 
+	 * @return the value of prenom
+	 */
+	public String getPrenom() {
+		return prenom;
+	}
 
-  /**
-   * Set the value of numero
-   * @param newVar the new value of numero
-   */
-  public void setNumero (int newVar) {
-    numero = newVar;
-  }
+	/**
+	 * Set the value of nom
+	 * 
+	 * @param newVar
+	 *            the new value of nom
+	 */
+	public void setNom(String newVar) {
+		nom = newVar;
+	}
 
-  /**
-   * Get the value of numero
-   * @return the value of numero
-   */
-  public int getNumero () {
-    return numero;
-  }
-  
-  /**
-   * Set the value of carte
-   * @param c
-   */
-  public void setCarteFidelite(CarteFidelite c) {
-	  carte = c;
-  }
-  
-  
-  /**
-   * Get the value of carte
-   * @return
-   */
-  public CarteFidelite getCarteFidelite() {
-	  return carte;
-  }
-  //
-  // Other methods
-  //
+	/**
+	 * Get the value of nom
+	 * 
+	 * @return the value of nom
+	 */
+	public String getNom() {
+		return nom;
+	}
 
+	/**
+	 * Set the value of numero
+	 * 
+	 * @param newVar
+	 *            the new value of numero
+	 */
+	public void setNumero(int newVar) {
+		numero = newVar;
+	}
+
+	/**
+	 * Get the value of numero
+	 * 
+	 * @return the value of numero
+	 */
+	public int getNumero() {
+		return numero;
+	}
+
+	/**
+	 * Set the value of carte
+	 * 
+	 * @param c
+	 */
+	public void setCarteFidelite(CarteFidelite c) {
+		carte = c;
+	}
+
+	/**
+	 * Get the value of carte
+	 * 
+	 * @return
+	 */
+	public CarteFidelite getCarteFidelite() {
+		return carte;
+	}
+
+	//
+	// Other methods
+	//
+	
+	/**
+	 * Add one Forfait to this Client
+	 * @param f
+	 */
+	public void addForfait(Forfait f) {
+		lesForfaits.add(f);
+	}
+	
+	
+	/**
+	 * get the List of Forfait for this Client
+	 * @return
+	 */
+	public ArrayList<Forfait> getForfait() {
+		return lesForfaits;
+	}
+	
 }
